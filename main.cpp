6679d46fe84c0c895d9b381fc87b238f7497e06a
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2023-06-21 22:46:37 -0400

    update

--- main.cpp
+++ main.cpp
@@ -2,25 +2,45 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "lib/graph2/include/program.hpp"
-#include "wayland-wlr-layer-shell-unstable-v1-client-protocol.h"
-static float vertices[] = {
-    -0.5, -0.5, 0.0,
-    0.0, 0.5, 0.0,
-    0.5, -0.5, 0.0,
+
+enum zwlr_layer_shell_v1_layer {
+	ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND = 0,
+	ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM = 1,
+	ZWLR_LAYER_SHELL_V1_LAYER_TOP = 2,
+	ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY = 3,
 };
+
+const float vertices[] = {
+    -1.0, -1.0, 0.0,  // Bottom left corner
+    -1.0, 1.0, 0.0,   // Top left corner
+    1.0, -1.0, 0.0,   // Bottom right corner
+    1.0, 1.0, 0.0     // Top right corner
+};
+
+// const unsigned int indices[] = {
+//     0, 1, 2,   // First triangle (bottom left, top left, bottom right)
+//     1, 3, 2    // Second triangle (top left, top right, bottom right)
+// };
+
+static unsigned int width = 1, height = 1;
+
+static Program *program = NULL;
+
 
 int main() {
     if (!glfwInit()) return 1;
     glfwDefaultWindowHints();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_WAYLAND_SHELL_LAYER, ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM);
+    // glfwWindowHint(GLFW_WAYLAND_SHELL_LAYER, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY);
     glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
     GLFWwindow *w = glfwCreateWindow(1 , 1,"krr",NULL,NULL);
     glfwMakeContextCurrent(w);
 
     glfwSetFramebufferSizeCallback(w, [] (GLFWwindow *wnd, int w, int h) {
         glViewport(0, 0, w, h);
+        width=w;
+        height=h;
     });
 
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -28,13 +48,22 @@ int main() {
     else
         printf("%s\n", glGetString(GL_VERSION));
 
-    Program p("main");
-    unsigned int vao, vbo;
+    program = new Program("main");
+    program->use();
+    program->setVec2("center", glm::vec2(width/2,height/2));
+    program->setFloat("radius", 0.5);
+    unsigned int vao, vbo, ebo;
     glCreateVertexArrays(1, &vao);
     glCreateBuffers(1, &vbo);
+    glCreateBuffers(1, &ebo);
+
     glBindVertexArray(vao);
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
             3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -42,8 +71,9 @@ int main() {
     while (!glfwWindowShouldClose(w)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.0,0.0,0.0,0.0);
-        p.use();
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        program->use();
+        program->setVec2("iRes", width, height);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
         glfwPollEvents();
         glfwSwapBuffers(w);
     }
--- res/shaders/main.frag
+++ res/shaders/main.frag
@@ -1,11 +1,18 @@
 #version 330 core
-out vec4 FragColor;
-// uniform vec4 color;
 
-in vec3 aPos;
+uniform vec2 center;
+uniform float radius;
+uniform vec2 iRes;
+
+out vec4 fragColor;
 
 void main()
 {
-	// FragColor = color;
-	FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec2 pos = gl_FragCoord.xy - center;
+    // Calculate the distance from the fragment to the center of the circle
+    float dist = sqrt(pos.x*pos.x + pos.y*pos.y);
+    if (dist < 100.0) 
+        fragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    else
+        fragColor = vec4(0.0);
 }
