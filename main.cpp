c45403a20c55eacd9630783ae89350c4a6e60661
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2023-06-22 03:29:25 -0400

    add clock

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <chrono>
+#include <ctime>
+
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "lib/graph2/include/program.hpp"
+
+#define NANOVG_GL3_IMPLEMENTATION
+#include "nanovg.h"
+#include "nanovg_gl.h"
+
 
 enum zwlr_layer_shell_v1_layer {
 	ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND = 0,
@@ -24,15 +32,12 @@ const float vertices[] = {
 
 static unsigned int width = 1, height = 1;
 
-static Program *program = NULL;
-
-
 int main() {
     if (!glfwInit()) return 1;
     glfwDefaultWindowHints();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    // glfwWindowHint(GLFW_WAYLAND_SHELL_LAYER, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY);
+    glfwWindowHint(GLFW_WAYLAND_SHELL_LAYER, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY);
     glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
     GLFWwindow *w = glfwCreateWindow(1 , 1,"krr",NULL,NULL);
     glfwMakeContextCurrent(w);
@@ -48,32 +53,77 @@ int main() {
     else
         printf("%s\n", glGetString(GL_VERSION));
 
-    program = new Program("main");
-    program->use();
-    program->setVec2("center", glm::vec2(width/2,height/2));
-    program->setFloat("radius", 0.5);
-    unsigned int vao, vbo, ebo;
-    glCreateVertexArrays(1, &vao);
-    glCreateBuffers(1, &vbo);
-    glCreateBuffers(1, &ebo);
+    NVGcontext *vg = nvglCreate(NVG_SRGB | NVG_AUTOW_DEFAULT);
+    
+    // Convert the time_t object to a local time struct
 
-    glBindVertexArray(vao);
+    auto f = [](int x){
+        float norm = (float)x/12;
+        float angle = norm*2*M_PI + 3*M_PI_2;
+        return angle;
+    };
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    auto g = [](int x){
+        float norm = (float)x/60;
+        float angle = norm*2*M_PI + 3*M_PI_2;
+        return angle;
+    };
+    
 
-
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
-            3*sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    int cx = 300, cy = 1400;
 
     while (!glfwWindowShouldClose(w)) {
+
+        auto now = std::chrono::system_clock::now();
+        // Convert the system time to a time_t object
+        std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+        struct std::tm* localTime = std::localtime(&currentTime);
+        
+        // Extract the hours and minutes from the local time struct
+        int hours = localTime->tm_hour;
+        int minutes = localTime->tm_min;
+
+
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.0,0.0,0.0,0.0);
-        program->use();
-        program->setVec2("iRes", width, height);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        nvgBeginFrame(vg, width, height, 1.0);
+
+
+        nvgFontSize(vg,200.0f);
+        nvgFontFace(vg,"sans-bold");
+        nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
+        nvgFillColor(vg,nvgRGBA(254,255,255,255));
+        nvgText(vg, cx + 400,cy ,"Hola",NULL);
+
+        nvgStrokeWidth(vg, 5.0f);
+        nvgStrokeColor(vg, nvgRGBA(254,255,255,254));
+
+        nvgBeginPath(vg);
+        nvgCircle(vg, cx, cy, 200);
+        nvgStroke(vg);
+
+        nvgFillColor(vg, nvgRGBA(0,0,0, 100));
+        nvgBeginPath(vg);
+        nvgCircle(vg, cx, cy, 200);
+        nvgFill(vg);
+
+        nvgBeginPath(vg);
+        nvgMoveTo(vg, cx + cos(f(hours))*0.5*200,
+                cy + sin(f(hours))*0.5*200);
+        nvgLineTo(vg, cx,cy);
+        nvgStroke(vg);
+
+        nvgBeginPath(vg);
+        nvgMoveTo(vg, cx + cos(g(minutes))*0.8*200,
+                cy + sin(g(minutes))*0.8*200);
+        nvgLineTo(vg, cx,cy);
+        nvgStroke(vg);
+
+
+
+
+        nvgEndFrame(vg);
+
         glfwPollEvents();
         glfwSwapBuffers(w);
     }
